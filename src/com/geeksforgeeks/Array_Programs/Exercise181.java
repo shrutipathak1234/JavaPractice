a94b242297862af8fa83647c79cc7723cf16116b
Shruti Pathak <shruti.pathak@D11-Shruti.Pathak>
2022-03-22 21:04:06 +0530

    added new programs

---
+++ src/com/geeksforgeeks/Array_Programs/Exercise181.java
@@ -0,0 +1,27 @@
+package com.geeksforgeeks.Array_Programs;
+
+public class Exercise181 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{50, 40, 30, 20, 10};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Array without sorting : " + arr[i]);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++)
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Array after sorting : " + arr[i]);
+
+        }
+
+    }
+}
