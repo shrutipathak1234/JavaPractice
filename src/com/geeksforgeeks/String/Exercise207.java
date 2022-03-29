0f171c27ae95d8b264fb84bf132aef9442e4eedb
Shruti Pathak <shruti.pathak@D11-Shruti.Pathak>
2022-03-29 20:42:55 +0530

    added new programs

--- src/com/geeksforgeeks/String/Exercise207.java
+++ src/com/geeksforgeeks/String/Exercise207.java
@@ -1,15 +1,41 @@
 package com.geeksforgeeks.String;
 // WAP to check whether a given string ends with the contents of another string.
-//String 1 : "Python Exercises"
-//String 2 : "se"
-//Output : false....... because String1 does not end with String2
+//         String 1 : "Python Exercises"
+//         String 2 : "se"
+//         Output : false....... because String1 does not end with String2
 public class Exercise207 {
-    public static void main(String[] args){
-    String s1="Python Exercises";
-    String s2="se";
-   s1.endsWith(s2);
+    static String s2 = "es";
+    public static void main(String[] args) {
 
+        String s1 = "Python Exercises";
+        String s3 = "";
 
+        for (int i = s1.length() - 1; i >= s1.length() - s2.length() ; i--) {
+            s3 = s3 + s1.charAt(i);
+        }
+        if (s3.equals(reverseString())){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
 
-   }
+    public static String reverseString() {
+
+            String s4="";
+            for(int i = s2.length()-1 ; i >=0 ; i--){
+                char c = s2.charAt(i);
+                s4 = s4 + c;
+        }
+            return s4;
+        }
 }
+
+
+
+
+
+
+
+
