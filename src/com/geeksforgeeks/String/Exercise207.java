41024d91d55d746efb92a35916210b7830bc8f4d
Shruti Pathak <shruti.pathak@D11-Shruti.Pathak>
2022-03-29 20:45:45 +0530

    added new programs

--- src/com/geeksforgeeks/String/Exercise207.java
+++ src/com/geeksforgeeks/String/Exercise207.java
@@ -4,33 +4,26 @@ package com.geeksforgeeks.String;
 //         String 2 : "se"
 //         Output : false....... because String1 does not end with String2
 public class Exercise207 {
-    static String s2 = "es";
+    static String s2 = "se";
     public static void main(String[] args) {
 
         String s1 = "Python Exercises";
         String s3 = "";
 
         for (int i = s1.length() - 1; i >= s1.length() - s2.length() ; i--) {
-            s3 = s3 + s1.charAt(i);
+            s3 =s1.charAt(i)+s3;
         }
-        if (s3.equals(reverseString())){
+        if(s3.equals(s2)){
             System.out.println("True");
         }
+
         else {
             System.out.println("False");
         }
     }
 
-    public static String reverseString() {
-
-            String s4="";
-            for(int i = s2.length()-1 ; i >=0 ; i--){
-                char c = s2.charAt(i);
-                s4 = s4 + c;
-        }
-            return s4;
-        }
 }
+
 
 
 
